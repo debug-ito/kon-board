1deaeb62b3cec7f2f4810b103d1cf23b7b7b9bdb
Toshio Ito <debug.ito@gmail.com>
2020-04-28 12:07:57 +0900

    write FromJSON and ToJSON for MealPhase.

--- kon-board/src/KonBoard/MealPlan.hs
+++ kon-board/src/KonBoard/MealPlan.hs
@@ -9,6 +9,9 @@ module KonBoard.MealPlan
     MealPlan(..)
   ) where
 
+import Control.Applicative (empty)
+import Data.Aeson (FromJSON(..), ToJSON(..), (.:), (.=))
+import qualified Data.Aeson as Aeson
 import Data.Text (Text)
 import Data.Time (Day)
 
@@ -16,11 +19,29 @@ import KonBoard.Recipe.Store (RecipeSummary)
 
 -- | Meal in a day.
 data MealPhase =
-  Breakfast
+    Breakfast
   | Lunch
   | Dinner
   | MealOther Text
   deriving (Show,Eq,Ord)
+
+instance FromJSON MealPhase where
+  parseJSON (Aeson.String s) =
+    case s of
+      "breakfast" -> return Breakfast
+      "lunch" -> return Lunch
+      "dinner" -> return Dinner
+      _ -> empty
+  parseJSON (Aeson.Object o) = MealOther <$> (o .: "meal_phase")
+  parseJSON _ = empty
+
+instance ToJSON MealPhase where
+  toJSON mp =
+    case mp of
+      Breakfast -> Aeson.String "breakfast"
+      Lunch -> Aeson.String "lunch"
+      Dinner -> Aeson.String "dinner"
+      MealOther p -> Aeson.object ["meal_phase" .= p]
 
 data MealPlan =
   MealPlan
