50cdcf04a01863a8be657c659992996dbfe9b08c
Toshio Ito <debug.ito@gmail.com>
2020-04-28 12:08:14 +0900

    start implemeting openYAMLs. WIP.

--- kon-board/src/KonBoard/MealPlan/Store.hs
+++ kon-board/src/KonBoard/MealPlan/Store.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE DeriveGeneric #-}
 -- |
 -- Module: KonBoard.MealPlan.Store
 -- Description: Storage service for MealPlans.
@@ -12,10 +13,15 @@ module KonBoard.MealPlan.Store
     openYAMLs
   ) where
 
-import Data.Time (Day)
+import Data.Aeson (FromJSON(..), ToJSON(..))
+import qualified Data.Aeson as Aeson
+import Data.Text (Text)
+import Data.Time (Day, fromGregorian)
+import GHC.Generics (Generic)
 
-import KonBoard.MealPlan (MealPlan)
+import KonBoard.MealPlan (MealPlan, MealPhase)
 import KonBoard.Recipe.Store (RecipeStore)
+import KonBoard.Recipe (Name)
 
 -- | Common API of 'MealPlan' store.
 class AMealPlanStore s where
@@ -33,3 +39,54 @@ instance AMealPlanStore YAMLStore where
 -- | Read YAML files to make a 'YAMLStore'.
 openYAMLs :: RecipeStore -> [FilePath] -> IO YAMLStore
 openYAMLs = undefined -- TODO
+
+aesonOpt :: Aeson.Options
+aesonOpt = Aeson.defaultOptions { Aeson.fieldLabelModifier = lmod }
+  where
+    lmod label = drop 3 label
+
+-- TODO
+
+---- fromMonthPlan :: RecipeStore -> MonthPlan -> IO [MealPlan]
+---- fromMonthPlan rstore mp = do
+---- 
+---- map toMP $ mp_plan mp
+----   where
+----     toMP dp =
+----       MealPlan
+----       { mealDay = fromGregorian (mp_year mp) (mp_month mp) (dp_d dp),
+----         mealPhase = dp_p dp,
+----         mealRecipe
+----       }
+
+data MonthPlan =
+  MonthPlan
+  { mp_year :: Integer,
+    mp_month :: Int,
+    mp_plan :: [DayPlan]
+  }
+  deriving (Show,Eq,Ord,Generic)
+
+instance FromJSON MonthPlan where
+  parseJSON = Aeson.genericParseJSON aesonOpt
+
+instance ToJSON MonthPlan where
+  toJSON = Aeson.genericToJSON aesonOpt
+  toEncoding = Aeson.genericToEncoding aesonOpt
+
+data DayPlan =
+  DayPlan
+  { dp_d :: Int,
+    dp_p :: MealPhase,
+    dp_m :: Name
+  }
+  deriving (Show,Eq,Ord,Generic)
+
+instance FromJSON DayPlan where
+  parseJSON = Aeson.genericParseJSON aesonOpt
+
+instance ToJSON DayPlan where
+  toJSON = Aeson.genericToJSON aesonOpt
+  toEncoding = Aeson.genericToEncoding aesonOpt
+
+
