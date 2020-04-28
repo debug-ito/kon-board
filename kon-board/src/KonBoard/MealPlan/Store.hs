1d442a2b401b962314f86eeac2544af865a719b9
Toshio Ito <debug.ito@gmail.com>
2020-04-28 13:54:49 +0900

    MealPlan.Store: implement YAMLStore. failing.

--- kon-board/src/KonBoard/MealPlan/Store.hs
+++ kon-board/src/KonBoard/MealPlan/Store.hs
@@ -15,13 +15,16 @@ module KonBoard.MealPlan.Store
 
 import Data.Aeson (FromJSON(..), ToJSON(..))
 import qualified Data.Aeson as Aeson
+import Data.List (sort)
 import Data.Text (Text)
 import Data.Time (Day, fromGregorian)
+import Data.Traversable (Traversable(..))
 import GHC.Generics (Generic)
 
-import KonBoard.MealPlan (MealPlan, MealPhase)
-import KonBoard.Recipe.Store (RecipeStore)
+import KonBoard.MealPlan (MealPlan(..), MealPhase)
+import KonBoard.Recipe.Store (RecipeStore, loadRecipeByName')
 import KonBoard.Recipe (Name)
+import KonBoard.Util.YAML (readYAMLDocs)
 
 -- | Common API of 'MealPlan' store.
 class AMealPlanStore s where
@@ -31,33 +34,35 @@ class AMealPlanStore s where
                   -> IO [MealPlan]
 
 -- | 'MealPlan' store based on YAML files.
-data YAMLStore = YAMLStore -- TODO
+newtype YAMLStore = YAMLStore [MealPlan]
 
 instance AMealPlanStore YAMLStore where
-  searchMealPlans = undefined -- TODO
+  searchMealPlans (YAMLStore mps) start end = return $ sort $ filter isInside mps
+    where
+      isInside mp = start <= mp_day && mp_day < end
+        where
+          mp_day = mealDay mp
 
 -- | Read YAML files to make a 'YAMLStore'.
 openYAMLs :: RecipeStore -> [FilePath] -> IO YAMLStore
-openYAMLs = undefined -- TODO
+openYAMLs rstore files = (fmap (YAMLStore . concat) . traverse (fromMonthPlan rstore))
+                         =<< (fmap concat $ traverse readYAMLDocs files)
 
 aesonOpt :: Aeson.Options
 aesonOpt = Aeson.defaultOptions { Aeson.fieldLabelModifier = lmod }
   where
     lmod label = drop 3 label
 
--- TODO
-
----- fromMonthPlan :: RecipeStore -> MonthPlan -> IO [MealPlan]
----- fromMonthPlan rstore mp = do
----- 
----- map toMP $ mp_plan mp
-----   where
-----     toMP dp =
-----       MealPlan
-----       { mealDay = fromGregorian (mp_year mp) (mp_month mp) (dp_d dp),
-----         mealPhase = dp_p dp,
-----         mealRecipe
-----       }
+fromMonthPlan :: RecipeStore -> MonthPlan -> IO [MealPlan]
+fromMonthPlan rstore mp = traverse toMP $ mp_plan mp
+  where
+    toMP dp = do
+      rsummary <- loadRecipeByName' rstore $ dp_m dp
+      return $ MealPlan
+               { mealDay = fromGregorian (mp_year mp) (mp_month mp) (dp_d dp),
+                 mealPhase = dp_p dp,
+                 mealRecipe = rsummary
+               }
 
 data MonthPlan =
   MonthPlan
