d44b3f9fc5c4da03c2ebea1460bee8c17f1005ec
Toshio Ito <debug.ito@gmail.com>
2020-04-30 13:46:51 +0900

    Now only the executable target depends on servant-elm
    servant-elm has a little heavy dep.

--- kon-board/app/ElmGenerate.hs
+++ kon-board/app/ElmGenerate.hs
@@ -1,4 +1,25 @@
-module Main (main) where
+module Main
+  ( main
+  ) where
 
-import KonBoard.Exec.ElmGenerate (main)
+import Data.Proxy (Proxy(..))
+import qualified Servant.Elm as Elm
+import System.Environment (getArgs)
 
+import KonBoard.Bridge.MealPlan (BMealPlan)
+import KonBoard.Bridge.Recipe (BRecipeSummary)
+import KonBoard.Web.API (GetMealPlans)
+
+main :: IO ()
+main = do
+  (dir : _) <- getArgs
+  Elm.generateElmModuleWith opts namespaces elm_imports dir defs api_proxy
+  where
+    opts = Elm.defElmOptions
+    namespaces = ["Bridge"]
+    defs =
+      [ Elm.DefineElm (Proxy :: Proxy BMealPlan),
+        Elm.DefineElm (Proxy :: Proxy BRecipeSummary)
+      ]
+    api_proxy = Proxy :: Proxy GetMealPlans
+    elm_imports = Elm.defElmImports
