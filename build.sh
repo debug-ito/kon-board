dbbadca2380a8f377734f3e4865693dc8a49e787
Toshio Ito <debug.ito@gmail.com>
2020-04-30 18:04:59 +0900

    build.sh: add building kon-elm.

--- build.sh
+++ build.sh
@@ -2,5 +2,9 @@
 
 thisdir=`dirname "$0"`
 
-stack test \
-    && stack run kon-board-gen-elm "$thisdir/kon-elm/src"
+echo "---- Build Haskell project" \
+    && stack test \
+    && echo "---- Generate Elm Bridge" \
+    && stack run kon-board-gen-elm "$thisdir/kon-elm/src" \
+    && echo "---- Build Elm application" \
+    && ( cd kon-elm; elm make src/Main.elm --output=../static/main.js )
