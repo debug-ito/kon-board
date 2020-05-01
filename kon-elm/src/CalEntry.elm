afeda9cd4a3dbfef34b846de9ce1387bcc46c3ce
Toshio Ito <debug.ito@gmail.com>
2020-05-01 16:58:01 +0900

    CalEntry: fix merge.

--- kon-elm/src/CalEntry.elm
+++ kon-elm/src/CalEntry.elm
@@ -65,7 +65,7 @@ merge bm cals  =
     ( \new_cal ->
           let f cal (acc, replaced) = if cal.day == new_cal.day && cal.phase == new_cal.phase
                                       then (new_cal :: acc, True)
-                                      else (acc, replaced)
+                                      else (cal :: acc, replaced)
               finalize (ret, replaced) = if replaced
                                          then ret
                                          else new_cal :: ret
