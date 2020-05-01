0a61dcf78951f84794493a6fa72335a9753d9a96
Toshio Ito <debug.ito@gmail.com>
2020-05-01 16:48:34 +0900

    add tests/CalEntrySpec.

---
+++ kon-elm/tests/CalEntrySpec.elm
@@ -0,0 +1,35 @@
+module CalEntrySpec exposing
+    ( suite
+    )
+
+{-| Unit tests -}
+
+import Expect as Exp
+import Date exposing (fromCalendarDate)
+import Test exposing (Test, describe, test)
+import Time
+
+import Bridge exposing (BMealPlan)
+import CalEntry
+import MealPhase exposing (MealPhase(..))
+
+suite : Test
+suite =
+    describe "CalEntry"
+        [ describe "merge"
+              [ test "merge to empty" <|
+                    \_ ->
+                    let input = { year = 2019, month = 4, day = 20
+                                , phase = "lunch"
+                                , recipe_summary = {id = "foo", name = "foo name"}
+                                }
+                        expected = Ok <|
+                                   [ { day = fromCalendarDate 2019 Time.Apr 20
+                                     , phase = Lunch
+                                     , recipeSummary = Just {id = "foo", name = "foo name"}
+                                     }
+                                   ]
+                        got = CalEntry.merge input []
+                    in Exp.equal got expected
+              ]
+        ]
