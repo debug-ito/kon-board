ddd74a830e14972f36c3e26f4a60e623e9195689
Toshio Ito <debug.ito@gmail.com>
2020-05-01 16:55:16 +0900

    CalEntrySpec: add single match case. fails.

--- kon-elm/tests/CalEntrySpec.elm
+++ kon-elm/tests/CalEntrySpec.elm
@@ -31,5 +31,79 @@ suite =
                                    ]
                         got = CalEntry.merge input []
                     in Exp.equal got expected
+              , test "merge to single match" <|
+                  \_ ->
+                      let bm = { year = 2019, month = 4, day = 20
+                               , phase = "lunch"
+                               , recipe_summary = {id = "foo", name = "foo name"}
+                               }
+                          cals = [ { day = fromCalendarDate 2019 Time.Apr 15
+                                   , phase = Lunch
+                                   , recipeSummary = Nothing
+                                   }
+                                 , { day = fromCalendarDate 2019 Time.Apr 15
+                                   , phase = Dinner
+                                   , recipeSummary = Nothing
+                                   }
+                                 , { day = fromCalendarDate 2019 Time.Apr 16
+                                   , phase = Lunch
+                                   , recipeSummary = Nothing
+                                   }
+                                 , { day = fromCalendarDate 2019 Time.Apr 16
+                                   , phase = Dinner
+                                   , recipeSummary = Nothing
+                                   }
+                                 , { day = fromCalendarDate 2019 Time.Apr 20
+                                   , phase = Lunch
+                                   , recipeSummary = Nothing
+                                   }
+                                 , { day = fromCalendarDate 2019 Time.Apr 20
+                                   , phase = Dinner
+                                   , recipeSummary = Nothing
+                                   }
+                                 , { day = fromCalendarDate 2019 Time.Apr 21
+                                   , phase = Lunch
+                                   , recipeSummary = Nothing
+                                   }
+                                 , { day = fromCalendarDate 2019 Time.Apr 21
+                                   , phase = Dinner
+                                   , recipeSummary = Nothing
+                                   }
+                                 ]
+                          expected = [ { day = fromCalendarDate 2019 Time.Apr 15
+                                       , phase = Lunch
+                                       , recipeSummary = Nothing
+                                       }
+                                     , { day = fromCalendarDate 2019 Time.Apr 15
+                                       , phase = Dinner
+                                       , recipeSummary = Nothing
+                                       }
+                                     , { day = fromCalendarDate 2019 Time.Apr 16
+                                       , phase = Lunch
+                                       , recipeSummary = Nothing
+                                       }
+                                     , { day = fromCalendarDate 2019 Time.Apr 16
+                                       , phase = Dinner
+                                       , recipeSummary = Nothing
+                                       }
+                                     , { day = fromCalendarDate 2019 Time.Apr 20
+                                       , phase = Lunch
+                                       , recipeSummary = Just {id = "foo", name = "foo name"}
+                                       }
+                                     , { day = fromCalendarDate 2019 Time.Apr 20
+                                       , phase = Dinner
+                                       , recipeSummary = Nothing
+                                       }
+                                     , { day = fromCalendarDate 2019 Time.Apr 21
+                                       , phase = Lunch
+                                       , recipeSummary = Nothing
+                                       }
+                                     , { day = fromCalendarDate 2019 Time.Apr 21
+                                       , phase = Dinner
+                                       , recipeSummary = Nothing
+                                       }
+                                     ]
+                          got = CalEntry.merge bm cals
+                      in Exp.equal got <| Ok expected
               ]
         ]
