26624cac6285b6e4b49a994d85e6db25e8fd96c5
Toshio Ito <debug.ito@gmail.com>
2020-05-17 10:34:11 +0900

    implement Locale, with global default locale.

--- kon-elm/src/Locale.elm
+++ kon-elm/src/Locale.elm
@@ -6,13 +6,14 @@ module Locale exposing
 
 {- | Locale support -}
 
-import Attr
 import Date exposing (Date, monthToNumber)
-import Html exposing (Html, text)
+import Html exposing (Html, text, div)
 import Html
+import Html.Attributes as Attr
 import String
-import Time exposing (Month)
+import Time exposing (Month, Weekday(..))
 
+import Bridge exposing (BIngredient)
 import MealPhase exposing (MealPhase(..))
 
 {- | Locale symbols.
@@ -22,8 +23,9 @@ type Locale = LJaJP
 {- | Locale-dependent functions.
 -}
 type alias LocaleImpl msg =
-    { viewDateLong : Date -> [Html msg]
-    , viewDateShort : Date -> [Html msg]
+    { viewDateLong : Date -> List (Html msg)
+    , viewDateShort : Date -> List (Html msg)
+    , viewIngredient : BIngredient -> List (Html msg)
     , showMealPhase : MealPhase -> String
     , showCalDay : String
     , showIngredients : String
@@ -41,12 +43,14 @@ localeJaJP =
     let result =
             { viewDateLong = jaViewDateLong
             , viewDateShort = jaViewDateShort
+            , viewIngredient = jaViewIngredient
             , showMealPhase = jaShowMealPhase
             , showCalDay = "日付"
             , showIngredients = "材料"
             , showRecipeSteps = "手順"
             , showRecipeReference = "参考"
             }
+        jaViewIngredient ing = [Html.li [] [text (ing.food ++ ": " ++ ing.qtty)]]
         jaShowMealPhase mp =
             case mp of
                 Breakfast -> "朝食"
@@ -55,7 +59,7 @@ localeJaJP =
                 MealOther s -> s
     in result
 
-jaViewDateLong : Date -> [Html msg]
+jaViewDateLong : Date -> List (Html msg)
 jaViewDateLong date =
     let result =
             [ div [] [ Html.span [Attr.class "clock-year", Attr.class "text-nowrap"]
@@ -68,13 +72,13 @@ jaViewDateLong date =
         year = (String.fromInt <| Date.year date) ++ "年"
     in result
 
-jaViewDateShort : Date -> [Html msg]
-jaViewDateShort _ = Debug.todo "todo"
+jaViewDateShort : Date -> List (Html msg)
+jaViewDateShort d = [text <| jaFormatDay d]
 
 jaFormatDay : Date -> String
 jaFormatDay d =
     let result = month ++ "/" ++ day ++ " " ++ weekday
-        month = String.fromInt <| monthToNumber d
+        month = String.fromInt <| monthToNumber <| Date.month d
         day = String.fromInt <| Date.day d
         weekday = "(" ++ (jaFormatWeekday <| Date.weekday d) ++ ")"
     in result
