06f54cfd7c760f620b6bd8daff10e8cdd6121c4d
Toshio Ito <debug.ito@gmail.com>
2022-03-14 21:48:14 +0900

    Bridge/MealPlan: ran stylish-haskell.

--- kon-board/src/KonBoard/Bridge/MealPlan.hs
+++ kon-board/src/KonBoard/Bridge/MealPlan.hs
@@ -1,38 +1,33 @@
 {-# LANGUAGE TemplateHaskell #-}
--- |
--- Module: KonBoard.Bridge.MealPlan
--- Description: Easy-to-encode MealPlan data type variant
--- Maintainer: Toshio Ito <debug.ito@gmail.com>
---
--- 
+-- | Easy-to-encode MealPlan data type variant
 module KonBoard.Bridge.MealPlan
-  ( BMealPlan,
-    toBMealPlan,
-    fromBMealPlan
-  ) where
+    ( BMealPlan
+    , toBMealPlan
+    , fromBMealPlan
+    ) where
 
-import Data.Foldable (toList)
-import Data.Text (Text)
-import Data.Time (toGregorian, fromGregorian)
-import qualified Elm.Derive as Elm
+import           Data.Foldable          (toList)
+import           Data.Text              (Text)
+import           Data.Time              (fromGregorian, toGregorian)
+import qualified Elm.Derive             as Elm
 
-import KonBoard.Bridge.Util (dropLabelOptions)
-import KonBoard.Bridge.Recipe
-  ( BRecipeSummary, toBRecipeSummary, fromBRecipeSummary
-  )
-import KonBoard.MealPlan (MealPlan(..), fromMealPhase, toMealPhase)
+import           KonBoard.Bridge.Recipe (BRecipeSummary, fromBRecipeSummary,
+                                         toBRecipeSummary)
+import           KonBoard.Bridge.Util   (dropLabelOptions)
+import           KonBoard.MealPlan      (MealPlan (..), fromMealPhase,
+                                         toMealPhase)
 
 -- | Easy-to-encode version of 'MealPlan'.
-data BMealPlan =
-  BMealPlan
-  { bm_year :: Integer,
-    bm_month :: Int,
-    bm_day :: Int,
-    bm_phase :: Text,
-    bm_recipes :: [BRecipeSummary],
-    bm_notes :: [Text]
-  }
-  deriving (Show,Eq,Ord)
+data BMealPlan
+  = BMealPlan
+      { bm_year    :: Integer
+      , bm_month   :: Int
+      , bm_day     :: Int
+      , bm_phase   :: Text
+      , bm_recipes :: [BRecipeSummary]
+      , bm_notes   :: [Text]
+      }
+  deriving (Eq, Ord, Show)
 
 toBMealPlan :: MealPlan -> BMealPlan
 toBMealPlan mp =
@@ -55,5 +50,5 @@ fromBMealPlan bm = do
                       mealRecipes = fmap fromBRecipeSummary $ bm_recipes bm,
                       mealNotes = bm_notes bm
                     }
-    
+
 $(Elm.deriveBoth (dropLabelOptions 3) ''BMealPlan)
