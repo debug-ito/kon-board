7b210536518094360055c81cbd2d499d10433aec
Toshio Ito <debug.ito@gmail.com>
2022-04-15 09:34:56 +0900

    Reorganize MealPlan module.

--- kon-board/src/KonBoard/MealPlan.hs
+++ kon-board/src/KonBoard/MealPlan.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE OverloadedStrings #-}
 -- |  Meal plan associated with recipes
 module KonBoard.MealPlan
     ( MealPlan (..)
@@ -8,18 +7,11 @@ module KonBoard.MealPlan
     , fromMealPhase
     ) where
 
-import           Control.Applicative   (empty)
-import           Data.Aeson            (FromJSON (..), ToJSON (..), (.:), (.=))
-import qualified Data.Aeson            as Aeson
-import           Data.Text             (Text)
-import qualified Data.Text             as T
-import           Data.Time             (Day)
+import qualified Data.Text       as T
+import           Data.Time       (Day)
 
-import           KonBoard.Recipe.Store (RecipeSummary)
-
--- $setup
---
--- >>> :set -XOverloadedStrings
+import           KonBoard.Base   (Text)
+import           KonBoard.Recipe (RecipeStored)
 
 -- | Time of a meal in a day.
 data MealPhase
@@ -30,15 +22,6 @@ data MealPhase
   deriving (Eq, Ord, Show)
 
 -- | Parse 'Text' into 'MealPhase'.
---
--- >>> toMealPhase "breakfast"
--- Right Breakfast
--- >>> toMealPhase "lunch"
--- Right Lunch
--- >>> toMealPhase "dinner"
--- Right Dinner
--- >>> toMealPhase "@other meal phase"
--- Right (MealOther "other meal phase")
 toMealPhase :: Text -> Either String MealPhase
 toMealPhase t =
   case t of
@@ -49,15 +32,6 @@ toMealPhase t =
          $ T.stripPrefix "@" t
 
 -- | Encode 'MealPhase' into 'Text'.
---
--- >>> fromMealPhase Breakfast
--- "breakfast"
--- >>> fromMealPhase Lunch
--- "lunch"
--- >>> fromMealPhase Dinner
--- "dinner"
--- >>> fromMealPhase (MealOther "foo bar")
--- "@foo bar"
 fromMealPhase :: MealPhase -> Text
 fromMealPhase mp =
   case mp of
@@ -72,9 +46,9 @@ type Note = Text
 -- | Plan of a meal.
 data MealPlan
   = MealPlan
-      { mealDay     :: Day
-      , mealPhase   :: MealPhase
-      , mealRecipes :: [RecipeSummary]
-      , mealNotes   :: [Note]
+      { day       :: Day
+      , mealPhase :: MealPhase
+      , recipes   :: [RecipeStored]
+      , notes     :: [Note]
       }
   deriving (Eq, Ord, Show)
---
+++ kon-board/test/KonBoard/MealPlanSpec.hs
@@ -0,0 +1,26 @@
+module KonBoard.MealPlanSpec
+    ( main
+    , spec
+    ) where
+
+import           Control.Monad     (forM_)
+import           Test.Hspec
+
+import           KonBoard.MealPlan (MealPhase (..), fromMealPhase, toMealPhase)
+
+main :: IO ()
+main = hspec spec
+
+spec :: Spec
+spec = do
+  describe "MealPhase" $ do
+    describe "toMealPhase / fromMealPhase" $ do
+      let testSet = [ (Breakfast, "breakfast")
+                    , (Lunch, "lunch")
+                    , (Dinner, "dinner")
+                    , (MealOther "other meal phase", "@other meal phase")
+                    ]
+      forM_ testSet $ \(mp, str) -> do
+        fromMealPhase mp `shouldBe` str
+        toMealPhase str `shouldBe` Right mp
+
