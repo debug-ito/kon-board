70434f1a16e85a1c75887cec79fcc0a407ff5b43
Toshio Ito <debug.ito@gmail.com>
2022-04-15 09:08:53 +0900

    Add skeleton of Memory.

---
+++ kon-board/src/KonBoard/Base.hs
@@ -0,0 +1,8 @@
+module KonBoard.Base
+    ( module X
+    ) where
+
+import           Data.ByteString     as X (ByteString)
+import           Data.HashMap.Strict as X (HashMap)
+import           Data.Monoid         as X (Monoid (..))
+import           Data.Semigroup      as X (Semigroup (..))
---
+++ kon-board/src/KonBoard/Recipe/Memory.hs
@@ -0,0 +1,131 @@
+module KonBoard.Recipe.Memory
+    ( RecipeStoreMemory (..)
+    , recipeStoreMemory
+    , readYaml
+    ) where
+
+import           KonBoard.Base   (ByteString, HashMap, Monoid (..), Semigroup (..))
+import           KonBoard.Recipe (Id, Name, RecipeStore (..), RecipeStored (..))
+
+-- | On-memory Recipe store.
+data RecipeStoreMemory
+  = RecipeStoreMemory
+      { fromId   :: HashMap Id RecipeStored
+      , fromName :: HashMap Name RecipeStored
+      }
+  deriving (Eq, Show)
+
+instance Semigroup RecipeStoreMemory where
+  a <> b = RecipeStoreMemory (fromId a <> fromId b) (fromName a <> fromName b)
+
+instance Monoid RecipeStoreMemory where
+  mappend = (<>)
+  mempty = RecipeStoreMemory mempty mempty
+
+recipeStoreMemory :: Applicative m => RecipeStoreMemory -> RecipeStore m
+recipeStoreMemory = undefined -- TODO
+
+readYaml :: (MonadLogger m, MonadThrow m) => ByteString -> m RecipeStoreMemory
+readYaml = undefined -- TODO
+
+
+
+---- -- | URL-fiendly ID for a recipe
+---- type ID = Text
+----
+---- -- | Consise summary of a 'Recipe'.
+---- data RecipeSummary
+----   = RecipeSummary
+----       { rsID   :: ID
+----       , rsName :: Name
+----       }
+----   deriving (Eq, Generic, Ord, Show)
+----
+---- data RecipeWithID
+----   = RecipeWithID
+----       { rwID     :: ID
+----       , rwRecipe :: Recipe
+----       }
+----   deriving (Eq, Ord, Show)
+----
+---- withIDToSummary :: RecipeWithID -> RecipeSummary
+---- withIDToSummary rw = RecipeSummary (rwID rw) (recipeName $ rwRecipe rw)
+----
+---- -- | Handle for the recipe storage.
+---- data RecipeStore
+----   = RecipeStore
+----       { fromID   :: HashMap ID RecipeWithID
+----       , fromName :: HashMap Name RecipeWithID
+----       }
+----
+---- emptyStore :: RecipeStore
+---- emptyStore = RecipeStore HM.empty HM.empty
+----
+---- makeID :: Name -> ID
+---- makeID = decodeUtf8 . Base16.encode . MD5.hash . encodeUtf8
+----
+---- -- | Exception about 'RecipeStore'.
+---- data RecipeStoreException
+----   = ConflictingID ID
+----   -- ^ ID conflict in the store.
+----   | ConflictingName Name
+----   -- ^ Name conflict in the store.
+----   | IDNotFound ID
+----   -- ^ ID is not found in the store.
+----   | NameNotFound Name
+----   -- ^ Recipe name is not found in the store.
+----   deriving (Eq, Ord, Show)
+----
+---- instance Exception RecipeStoreException
+----
+---- addRecipe :: Recipe -> RecipeStore -> Either RecipeStoreException RecipeStore
+---- addRecipe r rs = do
+----   when (HM.member rname $ fromName rs) $ do
+----     Left $ ConflictingName rname
+----   when (HM.member rid $ fromID rs) $ do
+----     Left $ ConflictingID rid
+----   Right $ rs { fromID = HM.insert rid rWithID $ fromID rs,
+----                fromName = HM.insert rname rWithID $ fromName rs
+----              }
+----   where
+----     rname = recipeName r
+----     rid = makeID rname
+----     rWithID = RecipeWithID rid r
+----
+---- toRecipeSummary :: ID -> Recipe -> RecipeSummary
+---- toRecipeSummary rid r = RecipeSummary rid $ recipeName r
+----
+---- loadRecipeByName :: RecipeStore -> Name -> IO (Maybe RecipeSummary)
+---- loadRecipeByName store name = return $ fmap withIDToSummary $ HM.lookup name $ fromName store
+----
+---- -- | Same as 'loadRecipeByName' except that 'Nothing' is converted to
+---- -- 'NameNotFound' exception.
+---- loadRecipeByName' :: RecipeStore -> Name -> IO RecipeSummary
+---- loadRecipeByName' rs n = maybe (throwIO $ NameNotFound n) return =<< loadRecipeByName rs n
+----
+---- loadRecipe :: RecipeStore -> ID -> IO Recipe
+---- loadRecipe store rid = maybe (throwIO $ IDNotFound rid) return $ fmap rwRecipe $ HM.lookup rid $ fromID store
+----
+---- loadRecipeSummary :: RecipeStore -> ID -> IO RecipeSummary
+---- loadRecipeSummary store rid = fmap (toRecipeSummary rid) $ loadRecipe store rid
+----
+---- toIO :: Exception e => Either e a -> IO a
+---- toIO (Right a) = return a
+---- toIO (Left e)  = throwIO e
+----
+---- -- | Open YAML files and make a 'RecipeStore'.
+---- openYAMLs :: (MonadLogger m, MonadIO m) => [FilePath] -> m RecipeStore
+---- openYAMLs files = do
+----   ref_store <- liftIO $ newIORef emptyStore
+----   forM_ files $ \file -> do
+----     logInfoN ("Read recipe YAML: " <> pack file)
+----     rs <- liftIO $ readRecipes file
+----     liftIO $ mapM_ (addR ref_store) rs
+----   liftIO $ readIORef ref_store
+----   where
+----     readRecipes file = (toIO . loadYAML) =<< BS.readFile file
+----     addR ref_store r = do
+----       store <- readIORef ref_store
+----       added <- toIO $ addRecipe r store
+----       writeIORef ref_store $ added
+
