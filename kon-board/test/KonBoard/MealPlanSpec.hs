6d2a308bd5cb72b96a442ba7fc25936a66a380c1
Toshio Ito <debug.ito@gmail.com>
2022-05-02 17:06:15 +0900

    WIP: modify MealPlanSpec.

--- kon-board/test/KonBoard/MealPlanSpec.hs
+++ kon-board/test/KonBoard/MealPlanSpec.hs
@@ -6,7 +6,7 @@ module KonBoard.MealPlanSpec
 import           Control.Monad     (forM_)
 import           Test.Hspec
 
-import           KonBoard.MealPlan (MealPhase (..), fromMealPhase, toMealPhase)
+import           KonBoard.MealPlan (MealPhase (..), MealPlanStore (..), fromMealPhase, toMealPhase)
 
 main :: IO ()
 main = hspec spec
@@ -23,4 +23,72 @@ spec = do
       forM_ testSet $ \(mp, str) -> do
         fromMealPhase mp `shouldBe` str
         toMealPhase str `shouldBe` Right mp
+
+
+
+specForStore :: String -> SpecWith (MealPlanStore m)
+specForStore storeName = describe storeName $ do
+  describe "searchMealPlans" $ do
+    specify "empty result" $ \s -> do
+      searchMealPlans s (day 2020 3 1) (day 2020 3 20) `shouldReturn` []
+    specify "exclusive range" $ \s -> do
+      let expected =
+            [ (day 2020 4 1, Lunch, ["recipe 1"], []),
+              (day 2020 4 2, Dinner, ["external recipe with URL"], [])
+            ]
+      got <- searchMealPlans s (day 2020 4 1) (day 2020 4 10)
+      map planWithoutID got `shouldBe` expected
+    specify "lunch and dinner" $ \s -> do
+      let expected =
+            [ (day 2020 4 2, Dinner, ["external recipe with URL"], []),
+              (day 2020 4 10, Lunch, ["recipe 1"], []),
+              (day 2020 04 10, Dinner, ["recipe 2"], [])
+            ]
+      got <- searchMealPlans s (day 2020 4 2) (day 2020 4 11)
+      map planWithoutID got `shouldBe` expected
+    specify "sorted by day and phase" $ \s -> do
+      let expected =
+            [ (day 2020 4 23, Lunch, ["recipe 2"], []),
+              (day 2020 4 23, Dinner, ["recipe 2"], []),
+              (day 2020 5 15, Breakfast, ["internal recipe with ingredient groups"], []),
+              (day 2020 5 15, Lunch, ["recipe 2"], [])
+            ]
+      got <- searchMealPlans s (day 2020 4 23) (day 2020 5 16)
+      map planWithoutID got `shouldBe` expected
+    specify "MealPlan with multiple recipes" $ \s -> do
+      let expected =
+            [ (day 2019 11 21, Dinner, ["recipe 1", "recipe 2"], [])
+            ]
+      got <- searchMealPlans s (day 2019 11 1) (day 2019 11 30)
+      map planWithoutID got `shouldBe` expected
+    specify "MealPlan in the example" $ \s -> do
+      let expected =
+            [ (day 2019 3 1, Lunch, ["Name of the meal"], []),
+              (day 2019 3 1, Dinner, ["Name of the meal", "Name of the external recipe"], []),
+              (day 2019 3 1, MealOther "late night snack", ["Name of the meal"], []),
+              (day 2019 3 2, Lunch, [], ["TODO: make this meal plan ASAP"]),
+              (day 2019 3 2, Dinner, ["Name of the meal"], ["Substitute sausages for bacons.", "TODO: buy ingredients"]),
+              (day 2019 4 1, Lunch, ["Name of the meal"], [])
+            ]
+      got <- searchMealPlans s (day 2019 3 1) (day 2019 5 1)
+      map planWithoutID got `shouldBe` expected
+    specify "MealPlan with MealOther phases" $ \s -> do
+      let expected =
+            [ (exp_date, Breakfast, ["recipe 1"], []),
+              (exp_date, Lunch, ["internal recipe with ingredient groups"], []),
+              (exp_date, Dinner, ["Name of the meal"], []),
+              (exp_date, MealOther "phase with at", ["recipe 2"], []),
+              (exp_date, MealOther "phase with bracket", ["external recipe with URL"], [])
+            ]
+          exp_date = day 2020 7 10
+      got <- searchMealPlans s (day 2020 7 10) (day 2020 7 11)
+      map planWithoutID got `shouldBe` expected
+    specify "MealPlan with notes and possible no recipes" $ \s -> do
+      let expected =
+            [ (exp_date, Lunch, ["recipe 1"], ["a note."]),
+              (exp_date, Dinner, [], ["multiple notes", "without recipe"])
+            ]
+          exp_date = day 2020 7 15
+      got <- searchMealPlans s (day 2020 7 15) (day 2020 7 16)
+      map planWithoutID got `shouldBe` expected
 
