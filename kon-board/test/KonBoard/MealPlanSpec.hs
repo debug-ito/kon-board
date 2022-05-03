d56c457241b6bb471e33eef9212c4bf03110f73e
Toshio Ito <debug.ito@gmail.com>
2022-05-03 21:39:51 +0900

    MealPlanSpec: use LoggingT IO

--- kon-board/test/KonBoard/MealPlanSpec.hs
+++ kon-board/test/KonBoard/MealPlanSpec.hs
@@ -4,14 +4,16 @@ module KonBoard.MealPlanSpec
     , specForStore
     ) where
 
-import           Control.Monad     (forM_)
-import           Data.Time         (Day, fromGregorian)
-import           GHC.Records       (HasField (..))
+import           Control.Monad        (forM_)
+import           Control.Monad.Logger (LoggingT)
+import           Data.Time            (Day, fromGregorian)
+import           GHC.Records          (HasField (..))
 import           Test.Hspec
 
-import           KonBoard.MealPlan (MealPhase (..), MealPlan (..), MealPlanStore (..), Note,
-                                    fromMealPhase, toMealPhase)
-import           KonBoard.Recipe   (Name, Recipe (..), RecipeStored (..))
+import           KonBoard.MealPlan    (MealPhase (..), MealPlan (..), MealPlanStore (..), Note,
+                                       fromMealPhase, toMealPhase)
+import           KonBoard.Recipe      (Name, Recipe (..), RecipeStored (..))
+import           KonBoard.TestLogger  (basicLogging)
 
 main :: IO ()
 main = hspec spec
@@ -64,18 +66,23 @@ planWithoutID mp = ( getField @"day" mp
 ----                    "plan_notes.yaml"
 ----                  ]
 
+loadYamls :: MealPlanStore (LoggingT IO) -> IO (MealPlanStore (LoggingT IO))
+loadYamls s = undefined -- TODO: load the above YAML files.
 
-specForStore :: String -> SpecWith (MealPlanStore IO)
-specForStore storeName = describe storeName $ do
+-- | Common set of tests for a 'MealPlanStore'. The user of this function should pass an empty
+-- 'MealPlanStore'.
+specForStore :: String -> SpecWith (MealPlanStore (LoggingT IO))
+specForStore storeName = beforeWith loadYamls $ describe storeName $ do
   describe "searchMealPlans" $ do
     specify "empty result" $ \s -> do
-      searchMealPlans s (gday 2020 3 1) (gday 2020 3 20) `shouldReturn` []
+      got <- basicLogging $ searchMealPlans s (gday 2020 3 1) (gday 2020 3 20)
+      got `shouldBe` []
     specify "exclusive range" $ \s -> do
       let expected =
             [ (gday 2020 4 1, Lunch, ["recipe 1"], []),
               (gday 2020 4 2, Dinner, ["external recipe with URL"], [])
             ]
-      got <- searchMealPlans s (gday 2020 4 1) (gday 2020 4 10)
+      got <- basicLogging $ searchMealPlans s (gday 2020 4 1) (gday 2020 4 10)
       map planWithoutID got `shouldBe` expected
     specify "lunch and dinner" $ \s -> do
       let expected =
@@ -83,7 +90,7 @@ specForStore storeName = describe storeName $ do
               (gday 2020 4 10, Lunch, ["recipe 1"], []),
               (gday 2020 04 10, Dinner, ["recipe 2"], [])
             ]
-      got <- searchMealPlans s (gday 2020 4 2) (gday 2020 4 11)
+      got <- basicLogging $ searchMealPlans s (gday 2020 4 2) (gday 2020 4 11)
       map planWithoutID got `shouldBe` expected
     specify "sorted by gday and phase" $ \s -> do
       let expected =
@@ -92,13 +99,13 @@ specForStore storeName = describe storeName $ do
               (gday 2020 5 15, Breakfast, ["internal recipe with ingredient groups"], []),
               (gday 2020 5 15, Lunch, ["recipe 2"], [])
             ]
-      got <- searchMealPlans s (gday 2020 4 23) (gday 2020 5 16)
+      got <- basicLogging $ searchMealPlans s (gday 2020 4 23) (gday 2020 5 16)
       map planWithoutID got `shouldBe` expected
     specify "MealPlan with multiple recipes" $ \s -> do
       let expected =
             [ (gday 2019 11 21, Dinner, ["recipe 1", "recipe 2"], [])
             ]
-      got <- searchMealPlans s (gday 2019 11 1) (gday 2019 11 30)
+      got <- basicLogging $ searchMealPlans s (gday 2019 11 1) (gday 2019 11 30)
       map planWithoutID got `shouldBe` expected
     specify "MealPlan in the example" $ \s -> do
       let expected =
@@ -109,7 +116,7 @@ specForStore storeName = describe storeName $ do
               (gday 2019 3 2, Dinner, ["Name of the meal"], ["Substitute sausages for bacons.", "TODO: buy ingredients"]),
               (gday 2019 4 1, Lunch, ["Name of the meal"], [])
             ]
-      got <- searchMealPlans s (gday 2019 3 1) (gday 2019 5 1)
+      got <- basicLogging $ searchMealPlans s (gday 2019 3 1) (gday 2019 5 1)
       map planWithoutID got `shouldBe` expected
     specify "MealPlan with MealOther phases" $ \s -> do
       let expected =
@@ -120,7 +127,7 @@ specForStore storeName = describe storeName $ do
               (exp_date, MealOther "phase with bracket", ["external recipe with URL"], [])
             ]
           exp_date = gday 2020 7 10
-      got <- searchMealPlans s (gday 2020 7 10) (gday 2020 7 11)
+      got <- basicLogging $ searchMealPlans s (gday 2020 7 10) (gday 2020 7 11)
       map planWithoutID got `shouldBe` expected
     specify "MealPlan with notes and possible no recipes" $ \s -> do
       let expected =
@@ -128,6 +135,6 @@ specForStore storeName = describe storeName $ do
               (exp_date, Dinner, [], ["multiple notes", "without recipe"])
             ]
           exp_date = gday 2020 7 15
-      got <- searchMealPlans s (gday 2020 7 15) (gday 2020 7 16)
+      got <- basicLogging $ searchMealPlans s (gday 2020 7 15) (gday 2020 7 16)
       map planWithoutID got `shouldBe` expected
 
