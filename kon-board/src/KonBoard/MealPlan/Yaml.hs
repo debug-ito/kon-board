779c875ca5d675550574b8663f43f47128b25c81
Toshio Ito <debug.ito@gmail.com>
2022-05-23 20:45:39 +0900

    start writing MealPlan.Yaml

--- kon-board/src/KonBoard/MealPlan/Yaml.hs
+++ kon-board/src/KonBoard/MealPlan/Yaml.hs
@@ -5,11 +5,13 @@ module KonBoard.MealPlan.Yaml
     , loadYamlFile
     ) where
 
-import qualified Data.ByteString   as BS
+import qualified Data.ByteString    as BS
 
-import           KonBoard.Base     (ByteString, MonadIO (..), MonadLogger, MonadThrow, traverse_)
-import           KonBoard.MealPlan (MealPlan, MealPlanStore (..))
-import           KonBoard.Recipe   (RecipeStore (..))
+import           KonBoard.Base      (ByteString, MonadIO (..), MonadLogger, MonadThrow, Text,
+                                     traverse_)
+import           KonBoard.MealPlan  (MealPlan, MealPlanStore (..))
+import           KonBoard.Recipe    (RecipeStore (..))
+import           KonBoard.Util.Yaml (ArrayOrSingle (..))
 
 readYamlFile :: (MonadLogger m, MonadThrow m, MonadIO m) => RecipeStore m -> FilePath -> m [MealPlan]
 readYamlFile r f = readYaml r =<< (liftIO $ BS.readFile f)
@@ -20,3 +22,19 @@ loadYamlFile m r f  = traverse_ (putMealPlan m) =<< readYamlFile r f
 readYaml :: (MonadLogger m, MonadThrow m) => RecipeStore m -> ByteString -> m [MealPlan]
 readYaml = undefined -- TODO
 
+data YMealPlan
+  = YMealPlan
+      { year  :: Int
+      , month :: Int
+      , plan  :: [YDayPlan]
+      }
+  deriving (Eq, Generic, ORd, Show)
+
+data YDayPlan
+  = YDayPlan
+      { d :: Int
+      , p :: Text
+      , m :: Maybe (ArrayOrSingle Text)
+      , n :: Maybe (ArrayOrSingle Text)
+      }
+  deriving (Eq, Generic, Ord, Show)
