6e12ce8caa75bf254eba96771a67cadbd2a3d392
Toshio Ito <debug.ito@gmail.com>
2022-06-20 21:49:40 +0900

    finish implementation of MealPlan.Memory

--- kon-board/src/KonBoard/MealPlan/Memory.hs
+++ kon-board/src/KonBoard/MealPlan/Memory.hs
@@ -5,11 +5,18 @@ module KonBoard.MealPlan.Memory
 import qualified Data.Map.Strict   as M
 import           Data.Time         (Day)
 
-import           KonBoard.Base     (HasField (..), MonadIO)
-import           KonBoard.MealPlan (MealPhase, MealPlan (..), MealPlanStore)
+import           KonBoard.Base     (HasField (..), MonadIO, liftIO, modifyIORef, newIORef,
+                                    readIORef)
+import           KonBoard.MealPlan (MealPhase, MealPlan (..), MealPlanStore (..))
 
 mealPlanStoreMemory :: (MonadIO m1, MonadIO m2) => m1 (MealPlanStore m2)
-mealPlanStoreMemory = undefined -- TODO
+mealPlanStoreMemory = do
+  refM <- liftIO $ newIORef M.empty
+  let putMP m = liftIO $ modifyIORef refM $ putMealPlanPure m
+      searchMP s e = liftIO $ searchMealPlansPure s e <$> readIORef refM
+  return $ MealPlanStore { putMealPlan = putMP
+                         , searchMealPlans = searchMP
+                         }
 
 type MealPlanMap = M.Map Key MealPlan
 
@@ -21,119 +28,8 @@ mkKey mp = (getField @"day" mp, getField @"phase" mp)
 putMealPlanPure :: MealPlan -> MealPlanMap -> MealPlanMap
 putMealPlanPure mp = M.insert (mkKey mp) mp
 
-searchMealPlansPure :: MealPlanMap -> Day -> Day -> [MealPlan]
-searchMealPlansPure mpm start end = M.elems $ M.takeWhileAntitone endCondition $ M.dropWhileAntitone startCondition mpm
+searchMealPlansPure :: Day -> Day -> MealPlanMap -> [MealPlan]
+searchMealPlansPure start end mpm = M.elems $ M.takeWhileAntitone endCondition $ M.dropWhileAntitone startCondition mpm
   where
     startCondition (d, _) = d < start
     endCondition (d, _) = d < end
-
----- -- | Common API of 'MealPlan' store.
----- class AMealPlanStore s where
-----   searchMealPlans :: s -- ^ 'MealPlan' store
-----                   -> Day -- ^ start date (inclusive)
-----                   -> Day -- ^ end date (exclusive)
-----                   -> IO [MealPlan]
-----
----- -- | 'MealPlan' store based on YAML files.
----- newtype YAMLStore
-----   = YAMLStore [MealPlan]
-----
----- instance AMealPlanStore YAMLStore where
-----   searchMealPlans (YAMLStore mps) start end = return $ sort $ filter isInside mps
-----     where
-----       isInside mp = start <= mp_day && mp_day < end
-----         where
-----           mp_day = mealDay mp
-----
----- -- | Read YAML files to make a 'YAMLStore'.
----- openYAMLs :: (MonadLogger m, MonadIO m) => RecipeStore -> [FilePath] -> m YAMLStore
----- openYAMLs rstore files = do
-----   yaml_docs <- fmap concat $ forM files $ \file -> do
-----     logInfoN ("Read meal plan YAML file: " <> pack file)
-----     liftIO $ readYAMLDocs file
-----   liftIO $ fmap (YAMLStore . concat) $ traverse (fromMonthPlan rstore) $ yaml_docs
-----
----- aesonOpt :: Aeson.Options
----- aesonOpt =
-----   Aeson.defaultOptions
-----   { Aeson.fieldLabelModifier = lmod,
-----     Aeson.omitNothingFields = True
-----   }
-----   where
-----     lmod label = drop 3 label
-----
----- fromMonthPlan :: RecipeStore -> MonthPlan -> IO [MealPlan]
----- fromMonthPlan rstore mp = traverse toMP $ mp_plan mp
-----   where
-----     toMP dp = do
-----       rsummaries <- traverse (loadRecipeByName' rstore) $ fromAOSToList $ dp_m dp
-----       return $ MealPlan
-----                { mealDay = fromGregorian (mp_year mp) (mp_month mp) (dp_d dp),
-----                  mealPhase = unYMealPhase $ dp_p dp,
-----                  mealRecipes = rsummaries,
-----                  mealNotes = fromAOSToList $ dp_n dp
-----                }
-----
----- fromAOSToList :: Maybe (ArrayOrSingle a) -> [a]
----- fromAOSToList = maybe [] F.toList
-----
----- data MonthPlan
-----   = MonthPlan
-----       { mp_year  :: Integer
-----       , mp_month :: Int
-----       , mp_plan  :: [DayPlan]
-----       }
-----   deriving (Eq, Generic, Ord, Show)
-----
----- instance FromJSON MonthPlan where
-----   parseJSON = Aeson.genericParseJSON aesonOpt
-----
----- instance ToJSON MonthPlan where
-----   toJSON = Aeson.genericToJSON aesonOpt
-----   toEncoding = Aeson.genericToEncoding aesonOpt
-----
----- type Meals = ArrayOrSingle Name
-----
----- type Notes = ArrayOrSingle Note
-----
----- data DayPlan
-----   = DayPlan
-----       { dp_d :: Int
-----       , dp_p :: YMealPhase
-----       , dp_m :: Maybe Meals
-----       , dp_n :: Maybe Notes
-----       }
-----   deriving (Eq, Generic, Ord, Show)
-----
----- instance FromJSON DayPlan where
-----   parseJSON = Aeson.genericParseJSON aesonOpt
-----
----- instance ToJSON DayPlan where
-----   toJSON = Aeson.genericToJSON aesonOpt
-----   toEncoding = Aeson.genericToEncoding aesonOpt
-----
----- -- | MealPhase for YAML encoding.
----- newtype YMealPhase
-----   = YMealPhase { unYMealPhase :: MealPhase }
-----   deriving (Eq, Ord, Show)
-----
----- instance FromJSON YMealPhase where
-----   parseJSON v = fmap YMealPhase $ parsedMealPhase
-----     where
-----       parsedMealPhase =
-----         case v of
-----           Aeson.String s -> either fail return $ toMealPhase s
-----           Aeson.Array a -> do
-----             guard (F.length a == 1)
-----             case F.toList a of
-----               [(Aeson.String p)] -> return $ MealOther p
-----               _                  -> empty
-----           _ -> empty
-----
----- instance ToJSON YMealPhase where
-----   toJSON (YMealPhase mp) =
-----     case mp of
-----       MealOther p -> toJSON [p]
-----       _           -> Aeson.String $ fromMealPhase mp
-
-
