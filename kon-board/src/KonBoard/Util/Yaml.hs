faa11e74d00bb62e91e049833ec365ef8d0118c9
Toshio Ito <debug.ito@gmail.com>
2022-12-09 23:31:53 +0900

    fix Util.Yaml

--- kon-board/src/KonBoard/Util/Yaml.hs
+++ kon-board/src/KonBoard/Util/Yaml.hs
@@ -9,13 +9,16 @@ module KonBoard.Util.Yaml
     ) where
 
 import           Control.Applicative    ((<$>), (<|>))
-import           Control.Exception.Safe (throwIO)
+import           Control.Exception.Safe (throwString)
+import           Data.Aeson             (FromJSON (..), ToJSON (..))
 import           Data.ByteString        (ByteString)
 import qualified Data.ByteString        as BS
 import qualified Data.ByteString.Char8  as BSC
+import qualified Data.ByteString.Lazy   as BSL
 import qualified Data.Foldable          as F
 import           Data.Word              (Word8)
-import           Data.YAML.Aeson        (decode1Strict)
+import           Data.YAML              (prettyPosWithSource)
+import           Data.YAML.Aeson        (decode1)
 import           GHC.Generics           (Generic)
 
 -- | (Internal use)
@@ -31,25 +34,19 @@ splitLineBS delim_line orig = map BSC.unlines $ splitByLine $ BSC.lines orig
                                   else (line : cur_group, ret)
         finalize (group, ret) = group : ret
 
-isSharpW8 :: Word8 -> Bool
-isSharpW8 w = w == 0x23
-
 isSpaceW8 :: Word8 -> Bool
 isSpaceW8 w = w == 0x09 || w == 0x0a || w == 0x0d || w == 0x20
-
-isAllSpace :: ByteString -> Bool
-isAllSpace s = BS.takeWhile isSpaceW8 s == s
 
 -- | Load data from YAML document, possibly encoded in \"multiple document\" encoding. The 'snd' of
 -- the result is the raw YAML document.
 decodeYAMLDocs :: FromJSON a => ByteString -> Either String [(a, ByteString)]
-decodeYAMLDocs doc = handleError $ traverse decodeWithRaw $ splitYAMLDocs doc
+decodeYAMLDocs doc = traverse decodeWithRaw $ map BSL.fromStrict $ splitYAMLDocs doc
   where
-    decodeWithRaw b = (,) <$> decode1Strict b <*> b
-    handleError e =
+    decodeWithRaw b = handleError b $ (,) <$> decode1 b <*> (pure $ BSL.toStrict b)
+    handleError input e =
       case e of
-        Right r = Right r
-        Left err = undefined -- TODO
+        Right r         -> Right r
+        Left (pos, err) -> Left $ prettyPosWithSource pos input (" error" ++ err)
 
 -- | Split the given into multiple blocks delimited by the line delimiter (@"---"@). Empty blocks
 -- are just dropped from the result.
@@ -59,7 +56,7 @@ splitYAMLDocs doc = filter (not . isEmptyDoc) $ splitLineBS "---" doc
     isEmptyDoc bs = BS.null $ BS.dropWhile isSpaceW8 bs
 
 readYAMLDocs :: FromJSON a => FilePath -> IO [(a, ByteString)]
-readYAMLDocs file = (either throwIO return . decodeYAMLDocs) =<< BS.readFile file
+readYAMLDocs file = (either throwString return . decodeYAMLDocs) =<< BS.readFile file
 
 
 -- | (internal use) A JSON/YAML encoding wrapper that is either a
