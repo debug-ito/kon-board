5df73f5090b08e6b6526de55afb8e4af79f432b3
Toshio Ito <debug.ito@gmail.com>
2023-01-03 14:51:58 +0900

    Recipe.MemorySpec: use TestStore module.

--- kon-board/test/KonBoard/Recipe/MemorySpec.hs
+++ kon-board/test/KonBoard/Recipe/MemorySpec.hs
@@ -3,27 +3,35 @@ module KonBoard.Recipe.MemorySpec
     , spec
     ) where
 
-import           Control.Monad          (void)
-import           Control.Monad.Logger   (LoggingT)
-import           Control.Monad.Trans    (MonadIO (..))
-import           Data.Foldable          (traverse_)
-import           Data.List              (nub)
-import           Data.Maybe             (fromJust)
-import qualified Data.Text              as T
-import           GHC.Records            (HasField (..))
+import           Control.Monad.Logger      (LoggingT)
+import           Control.Monad.Trans       (MonadIO (..))
+import           Data.Foldable             (traverse_)
+import           Data.List                 (nub)
+import           Data.Maybe                (fromJust)
+import           GHC.Records               (HasField (..))
 import           Test.Hspec
 
-import           KonBoard.Recipe        (Id, Name, RecipeStore (..), RecipeStored (..))
-import           KonBoard.Recipe.Memory (recipeStoreMemory)
-import           KonBoard.Recipe.Yaml   (loadYamlFile, parseRecipe)
+import           KonBoard.Recipe           (Id, Name, RecipeStore (..), RecipeStored (..))
+import           KonBoard.Recipe.Memory    (recipeStoreMemory)
+import           KonBoard.Recipe.Yaml      (loadYamlFile)
 
-import           KonBoard.TestLogger    (basicLogging)
+import           KonBoard.Recipe.TestStore (makeRecipeStoreSpec)
+import           KonBoard.TestLogger       (basicLogging)
 
 main :: IO ()
 main = hspec spec
 
 spec :: Spec
-spec = specStore
+spec = do
+  makeRecipeStoreSpec "recipeStoreMemory" recipeStoreMemory
+  describe "recipeStoreMemory" $ do
+    specify "recipeID has to be stable regardless of store" $ basicLogging $ do
+      let input = [ ["recipe_multi.yaml"],
+                    ["recipe_in.yaml", "recipe_url.yaml", "recipe_in_url.yaml", "recipe_multi.yaml"],
+                    ["recipe_url.yaml", "recipe_in.yaml", "recipe_multi.yaml", "recipe_in_url.yaml"]
+                  ]
+      got <- traverse (idForStore "recipe 2") $ input
+      liftIO $ (length $ nub got) `shouldBe` 1
 
 openStore :: [FilePath] -> LoggingT IO (RecipeStore (LoggingT IO))
 openStore files = do
@@ -36,47 +44,3 @@ idForStore n files = do
   store <- openStore $ files
   fmap (getField @"id" . fromJust) $ getRecipeByName store n
 
-loadAndCheckName :: (MonadIO m, MonadFail m) => RecipeStore m -> Name -> m ()
-loadAndCheckName store inputName = do
-  (Just rs) <- getRecipeByName store inputName
-  liftIO $ (getField @"name" $ getField @"recipe" rs) `shouldBe` inputName
-  let rid = getField @"id" rs
-  (Just rsById) <- getRecipeById store rid
-  liftIO $ rsById `shouldBe` rs
-  liftIO $ putStrLn ("Recipe: '" <> T.unpack inputName <> "' -> ID: " <> T.unpack rid)
-
-specStore :: Spec
-specStore = describe "recipeStoreMemory" $ do
-  let commonYamlFiles = [ "recipe_in.yaml"
-                        , "recipe_in_url.yaml"
-                        , "recipe_multi.yaml"
-                        , "recipe_url.yaml"
-                        ]
-  specify "getRecipeByName, getRecipeById" $ basicLogging $ do
-    store <- openStore commonYamlFiles
-    loadAndCheckName store "internal recipe with ingredient groups"
-    loadAndCheckName store "external recipe with URL"
-    loadAndCheckName store "recipe 2"
-  specify "loadRecipeByName, not found" $ basicLogging $ do
-    store <- openStore commonYamlFiles
-    got <- getRecipeByName store "this should not exist"
-    liftIO $ got `shouldBe` Nothing
-  specify "recipeID has to be stable regardless of store" $ basicLogging $ do
-    let input = [ ["recipe_multi.yaml"],
-                  ["recipe_in.yaml", "recipe_url.yaml", "recipe_in_url.yaml", "recipe_multi.yaml"],
-                  ["recipe_url.yaml", "recipe_in.yaml", "recipe_multi.yaml", "recipe_in_url.yaml"]
-                ]
-    got <- traverse (idForStore "recipe 2") $ input
-    liftIO $ (length $ nub got) `shouldBe` 1
-  specify "updateRecipe" $ basicLogging $ do
-    store <- openStore commonYamlFiles
-    (Just old) <- getRecipeByName store "recipe 1"
-    let oldId = getField @"id" old
-        newRecipeYaml = "name: recipe 1\ndesc: hoge hoge\n"
-        (Right newRecipe) = parseRecipe newRecipeYaml
-        newStored = RecipeStored { id = oldId, recipe = newRecipe }
-    void $ updateRecipe store newStored
-    gotByName <- getRecipeByName store "recipe 1"
-    liftIO $ gotByName `shouldBe` Just newStored
-    gotById <- getRecipeById store oldId
-    liftIO $ gotById `shouldBe` Just newStored
