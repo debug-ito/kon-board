dc6f01bb18ab1144a8b0e70b1fb34f891c1af204
Toshio Ito <debug.ito@gmail.com>
2023-01-21 22:59:52 +0900

    Fix MealPlan.Memory.

--- kon-board/src/KonBoard/MealPlan/Memory.hs
+++ kon-board/src/KonBoard/MealPlan/Memory.hs
@@ -3,33 +3,40 @@ module KonBoard.MealPlan.Memory
     ) where
 
 import qualified Data.Map.Strict   as M
+import qualified Data.Text         as T
 import           Data.Time         (Day)
 
-import           KonBoard.Base     (HasField (..), MonadIO, liftIO, modifyIORef, newIORef,
-                                    readIORef)
+import           KonBoard.Base     (HasField (..), MonadIO, MonadThrow, liftIO, modifyIORef,
+                                    newIORef, readIORef, throwString)
 import           KonBoard.MealPlan (MealPhase, MealPlan (..), MealPlanStore (..))
+import           KonBoard.Recipe   (Id, RecipeStore (getRecipeById), RecipeStored)
 
-mealPlanStoreMemory :: (MonadIO m1, MonadIO m2) => m1 (MealPlanStore m2)
-mealPlanStoreMemory = do
+mealPlanStoreMemory :: (MonadIO m1, MonadIO m2, MonadThrow m2) => RecipeStore m2 -> m1 (MealPlanStore m2)
+mealPlanStoreMemory rStore = do
   refM <- liftIO $ newIORef M.empty
-  let putMP m = liftIO $ modifyIORef refM $ putMealPlanPure m
-      searchMP s e = liftIO $ searchMealPlansPure s e <$> readIORef refM
+  let putMP m = liftIO $ modifyIORef refM $ putMealPlanImpl m
+      searchMP s e = searchMealPlansImpl rStore s e =<< (liftIO $ readIORef refM)
   return $ MealPlanStore { putMealPlan = putMP
                          , searchMealPlans = searchMP
                          }
 
-type MealPlanMap = M.Map Key MealPlan
+type MealPlanMap = M.Map Key (MealPlan Id)
 
 type Key = (Day, MealPhase)
 
-mkKey :: MealPlan -> Key
+mkKey :: MealPlan r -> Key
 mkKey mp = (getField @"day" mp, getField @"phase" mp)
 
-putMealPlanPure :: MealPlan -> MealPlanMap -> MealPlanMap
-putMealPlanPure mp = M.insert (mkKey mp) mp
+putMealPlanImpl :: MealPlan Id -> MealPlanMap -> MealPlanMap
+putMealPlanImpl mp = M.insert (mkKey mp) mp
 
-searchMealPlansPure :: Day -> Day -> MealPlanMap -> [MealPlan]
-searchMealPlansPure start end mpm = M.elems $ M.takeWhileAntitone endCondition $ M.dropWhileAntitone startCondition mpm
+searchMealPlansImpl :: MonadThrow m => RecipeStore m -> Day -> Day -> MealPlanMap -> m [MealPlan RecipeStored]
+searchMealPlansImpl rStore start end mpm = traverse (resolveRecipe rStore) $ M.elems $ M.takeWhileAntitone endCondition $ M.dropWhileAntitone startCondition mpm
   where
     startCondition (d, _) = d < start
     endCondition (d, _) = d < end
+
+resolveRecipe :: MonadThrow m => RecipeStore m -> MealPlan Id -> m (MealPlan RecipeStored)
+resolveRecipe rStore mp = traverse getRecipeByIdThrowing mp
+  where
+    getRecipeByIdThrowing i = maybe (throwString ("recipe ID not found: " <> T.unpack i)) return =<< getRecipeById rStore i
