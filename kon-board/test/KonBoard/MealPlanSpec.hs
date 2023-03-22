2050ec442a6115e126b2cf135196bd76e4b0d452
Toshio Ito <debug.ito@gmail.com>
2023-03-22 10:26:21 +0900

    MealPlanSpec: rearrange the test structure a little.

--- kon-board/test/KonBoard/MealPlanSpec.hs
+++ kon-board/test/KonBoard/MealPlanSpec.hs
@@ -14,12 +14,12 @@ main = hspec spec
 spec :: Spec
 spec = do
   describe "MealPhase" $ do
-    specify "toMealPhase / fromMealPhase" $ do
+    describe "toMealPhase / fromMealPhase" $ do
       let testSet = [ (Breakfast, "breakfast")
                     , (Lunch, "lunch")
                     , (Dinner, "dinner")
                     , (MealOther "other meal phase", "@other meal phase")
                     ]
-      forM_ testSet $ \(mp, str) -> do
+      forM_ testSet $ \(mp, str) -> specify (show mp) $ do
         fromMealPhase mp `shouldBe` str
         toMealPhase str `shouldBe` Right mp
