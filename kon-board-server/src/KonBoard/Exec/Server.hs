a75b38bb27e5c0d53f3d47e79f1141ae953df1fd
Toshio Ito <debug.ito@gmail.com>
2023-03-30 22:57:54 +0900

    Exec.Server: add condition about KON_BOARD_NO_INIT_DB_AT_START.

--- kon-board-server/src/KonBoard/Exec/Server.hs
+++ kon-board-server/src/KonBoard/Exec/Server.hs
@@ -4,17 +4,23 @@ module KonBoard.Exec.Server
     ) where
 
 import           Control.Exception.Safe   (bracket)
+import           Control.Monad            (when)
 import           Control.Monad.IO.Class   (MonadIO (..))
 import           Control.Monad.Logger     (logInfoN, runStderrLoggingT)
 import           Data.Monoid              ((<>))
 import           Data.Text                (pack)
 import           Network.Wai.Handler.Warp (run)
+import           System.Environment       (lookupEnv)
 
 import           KonBoard.Web.App         (appWith, closeKonApp, initDb, newKonApp)
 
 main :: IO ()
 main = runStderrLoggingT $ do
-  initDb
+  let noInitEnvName = "KON_BOARD_NO_INIT_DB_AT_START"
+  noInitAtStart <- liftIO $ fmap (maybe "" id) $ lookupEnv noInitEnvName
+  if (noInitAtStart == "")
+    then initDb
+    else logInfoN ("environment variable " <> pack noInitEnvName <> " is set. Skip initializing the DB...")
   bracket newKonApp closeKonApp $ \server -> do
     let port = 8888 :: Int
     logInfoN ("Listen on port " <> (pack $ show port))
