51487549e0bd81229251a1a96d9230c881ba9d5e
Toshio Ito <debug.ito@gmail.com>
2023-08-02 22:52:45 +0900

    elm: PageSpec: add test cases for linkHashtagsMarkdown.

--- kon-elm/tests/PageSpec.elm
+++ kon-elm/tests/PageSpec.elm
@@ -18,7 +18,7 @@ parseUrl_ s = Url.fromString s |> Maybe.andThen
               )
 
 linkHashtags_ : String -> String
-linkHashtags_ = Page.linkHashtagsMarkdown (\t -> "/q=#" ++ t)
+linkHashtags_ = Page.linkHashtagsMarkdown (\t -> "/?q=#" ++ t)
 
 suite : Test
 suite =
@@ -41,9 +41,15 @@ suite =
              [ test "empty" <|
                  \_ -> Exp.equal (linkHashtags_ "") ""
              , test "a hashtag" <|
-                 \_ -> Exp.equal (linkHashtags_ "#hoge") "[#hoge](/q=#hoge)"
+                 \_ -> Exp.equal (linkHashtags_ "#hoge") "[#hoge](/?q=#hoge)"
              , test "multiple hashtags" <|
-                 \_ -> Exp.equal (linkHashtags_ "#foo bar buzz #quux #hoge") "[#foo](/q=#foo) bar buzz [#quux](/q=#quux) [#hoge](/q=#hoge)"
+                 \_ -> Exp.equal (linkHashtags_ "#foo bar buzz #quux #hoge") "[#foo](/?q=#foo) bar buzz [#quux](/?q=#quux) [#hoge](/?q=#hoge)"
+             , test "hashtag at the start of line" <|
+                 \_ -> Exp.equal (linkHashtags_ "quux\n#foobar\n#buzz") "quux\n[#foobar](/?q=foobar)\n[#buzz](/?q=buzz)"
+             , test "Japanese hashtag" <|
+                 \_ -> Exp.equal (linkHashtags_ "  #日本語okハッシュタグ ") "  [#日本語okハッシュタグ](/?q=%E6%97%A5%E6%9C%AC%E8%AA%9Eok%E3%83%8F%E3%83%83%E3%82%B7%E3%83%A5%E3%82%BF%E3%82%B0) "
+             , test "hash in hashtag" <|
+                 \_ -> Exp.equal (linkHashtags_ " #hoge#fuga") " #hoge#fuga"
              , test "markdown headings" <|
                  \_ -> let input = "# heading 1\n\n## heading 2\n\n### heading 3"
                        in Exp.equal (linkHashtags_ input) input
