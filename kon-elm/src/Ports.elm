6caaaa45e4ed708da5ba6b722aefb7b793866b50
Toshio Ito <debug.ito@gmail.com>
2023-10-31 23:22:59 +0900

    elm: add portSelectElement

--- kon-elm/src/Ports.elm
+++ kon-elm/src/Ports.elm
@@ -1,7 +1,10 @@
 port module Ports exposing
     ( portOnScroll
+    , portSelectElement
     )
 
 import Json.Decode exposing (Value)
 
 port portOnScroll : (Value -> msg) -> Sub msg
+
+port portSelectElement : String -> Cmd msg
